780da7bd86b0db72c77ea5f8078addac9195aa67
Shah <pallav.shah@capgemini.com>
2018-09-28 22:34:19 -0500

    Network Class

---
+++ app/src/main/java/com/example/pshah1/flickerapi/utlis/NetworkUtils.java
@@ -0,0 +1,51 @@
+package com.example.pshah1.flickerapi.utlis;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+
+    public final static String BASE_URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8eef5bcc246a9e8d6973f008baec24b3&text=chicago&safe_search=1&format=json&nojsoncallback=1&extras=url_m";
+    private final static String TAG = NetworkUtils.class.getSimpleName();
+
+    public static URL buildURL(String flickerStringUrl){
+        Uri uri = Uri.parse(flickerStringUrl); // parse string to URL
+
+        URL url = null;
+        try {
+            url = new URL(uri.toString());
+        }catch (Exception e){
+           e.printStackTrace();
+        }
+
+        Log.w(TAG, "Build URL -----------------------------------------" + url.toString());
+
+        return url;
+    }
+
+    public static String getResponse(URL buildUrl) throws IOException {
+        // Make HTTP connection - establish connection
+        HttpURLConnection urlConnection = (HttpURLConnection) buildUrl.openConnection();
+
+        try {
+         InputStream inputStream =  urlConnection.getInputStream();
+            Scanner scanner = new Scanner(inputStream);
+            scanner.useDelimiter("\\A");
+
+            if (scanner.hasNext()){
+                return scanner.next();
+            }else {
+                return null;
+            }
+        }finally {
+            urlConnection. disconnect();
+        }
+
+    }
+}
